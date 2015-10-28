31ed1effe98d9a2bc0f2454a2c5dbf8ec193c05e
doungni <2303134@qq.com>
2015-10-28 16:17:52 +0800

    modify

--- shell/checkNetwork.sh
+++ shell/checkNetwork.sh
@@ -11,14 +11,11 @@
 function checkNetwork() {
     timeout=7
     target="www.bitbucket.org"
-
     # variable and no variable 
     ret=`curl -I -s --connect-timeout $timeout $target -w %{http_code} | tail -n1`
     #ret=`curl -I -s --connect-timeout 7 bitbucket.org -w %{http_code} | tail -n1`
-
     # print ret
     echo "$ret"
-
     # judge 301 and 302, avoid github.com/www.github.com,http_code reture 301 & 302
     # judge http return code:200, regula no goto
     if [ "$ret" = "302" ] || [ "$ret" = "301" ] || [ "$ret" = "200" ]; then
