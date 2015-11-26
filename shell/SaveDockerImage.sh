5260135288de729b7656e86b534e3de41b800866
doungni <2303134@qq.com>
2015-11-26 15:16:34 +0800

    save docker image v1

--- shell/SaveDockerImage.sh
+++ shell/SaveDockerImage.sh
@@ -1,66 +1,79 @@
 #!/bin/bash -xe
 ################################################################################################
-# * Author        : doungni
-# * Email         : doungni@doungni.com
-# * Last modified : 2015-11-24 00:46
-# * Filename      : SaveDockerImage.sh
-# * Description   : 
+# * Author         : doungni
+# * Email          : doungni@doungni.com
+# * Last modified  : 2015-11-24 00:46
+# * Filename       : SaveDockerImage.sh
+# * Description    : 
 ################################################################################################
 
 ################################################################################################
-# * Parameter     :
-#   image_path    : By jenkins job configuration
-#   image_name    : By jenkins job configuration
-#   splitfile_md5 : By jenkins job configuration
+# * Parameter         :
+#   docker_image_name : By jenkins job configuration
+#   docker_image_path : By jenkins job configuration
+#   docker_daemon_port: By jenkins job configuration
+#   split_file_size   : By jenkins job configuration
+#   file_md5_path     : ${image_path}/file_md5_path
 #
 ################################################################################################
 
-if [ -z ${image_path} ]; then
-    image_path=`mkdir -p ~/docker_file`
-fi
-
-if [ -z ${image_name} ]; then
-    image_name="iam_docker.tar.gz"
-fi
 
 # print log message
 function log() {
     local msg
-    echo `data +'[%Y-%m-%d %H-%M-%S']` "\n $msg\n"
+    echo `date +'[%Y-%m-%d %H-%M-%S']` "\n $msg\n"
 }
 
-# image host ssh port
-ssh_port=2703
-# docker daemon ip
-daemon_ip="172.17.42.1"
+# recognize docker daemon ip
+# by current known daemon ip judge
+function recognize_docker_daemon_ip() {
+    local docker_daemon_ip=""
+    maybe_ip="172.17.0.1 172.17.42.1 172.18.0.1 172.18.42.1 192.168.50.10 192.168.99.1"
+    maybe_ip=($maybe_ip)
+    for ip in ${maybe_ip[*]}; do
+	if ping -c3 $ip 2>/dev/null 1>/dev/null; then
+	    docker_daemon_ip=$ip
+	    break
+	fi
+    done
+    
+    echo $docker_daemon_ip
+}
 
-function save_image() {
-    local split_image
-    local 
+# save docker image, by ssh method
+function save_docker_image() {
 
     # from jenkins container to docker daemon server
-    ssh_return=`ssh -p ${ssh_port} root@ ${daemon_ip} "df -h" && echo yes || echo no`
-    
-    if [ "x${ssh_return}" = "xyes" ]; then
-        # save docker image
-        #docker save ${docker_image} > iam_docker.tar.gz
-        cd ${image_path}
-    
-        if [ -e ${image_name} ]; then
-            # split_images="iam_docker.tar.gz"
+    ssh -tt -p ${docker_daemon_port} root@${docker_daemon_ip} <<EOF
+        mkdir -p ${docker_image_path}
 
-            split_iam=`split -b 314572800 ${image_name} split_image`
-    
-            for split_image in ${split_iam[*]}
-            do
-                md5check split_image >> split_iam.md5
+        cd ${docker_image_path}
+	    mkdir -p ${docker_image_name%%.*}_${datefile}
 
-                cp split_image* split_iam.md5 ${splitfile_md5}
-            done
+        if [ -r ${docker_image_name} ]; then
+	        cd ${docker_image_name%%.*}_${datefile}
+            split -b ${file_size} ../${docker_image_name} ${docker_image_name%%.*}
+	        md5sum \`ls ${docker_image_name%%.*}*\` > ${docker_image_name%%.*}.md5
         else
-            log "####### ${image_name} can not found in ${image_path} path #######"
+	        log "The ${image_name} can not found in the ${docker_image_path}"
         fi
-    else
-        log "####### Can not collect docker daemon host, Please check you config #######"
-    fi
+        exit 1
+EOF
 }
+
+################################## Entrance  ##############################################
+
+# each file split bytes
+file_size=$(( ${split_file_size} * 1024 * 1024 ))
+
+datefile=`date +%Y-%m-%d`
+
+docker_daemon_ip=$(recognize_docker_daemon_ip)
+if [ -z $docker_daemon_ip ]; then
+    echo "ERROR: failed to recognize docker daemon ip"
+    exit 1
+else 
+    echo "docker_daemon_ip: ${docker_daemon_ip}"
+fi
+
+save_docker_image
