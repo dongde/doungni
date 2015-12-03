f29768de13c0ab11f607adca42aad53337195235
doungni <2303134@qq.com>
2015-12-03 14:23:57 +0800

    modify logic

--- shell/collect_logfile.sh
+++ shell/collect_logfile.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -xe
+#!/bin/bash -e
 #######################################################################################
 #  Author        : doungni
 #  Email         : doungni@doungni.com
@@ -43,22 +43,22 @@ function log() {
 function collect_logfile() {
     # Non NULL judgment of ${server_list}
     if [ -z "${server_list}" ]; then
-        log "Please refer to the correct parameters for the prompt configuration"
+        log "Error: Please refer to the correct parameters for the prompt configuration"
         exit 1
     fi
 
     # Each server is separated by a comma
     server_arr=(${server_list//,/ })
 
-    # Count collecting log on the server, use variable m
-    local count_server=0
+    # Count collecting log on the server, use variable count_machine
+    local count_machine=0
     for i in ${server_arr[*]}
     do
-        count_server=`expr ${count_server} + 1`
-        echo -e "\nNeed to collect the logfile Server ${count_server}:\nIp:Port\n$i\n"
+        count_machine=`expr ${count_machine} + 1`
+        echo -e "Need to collect the log file Server Machine ${count_machine}:\nIp:Port\n$i\n"
     done
 
-    # Count current traversal of the server, use variable n
+    # Count current traversal of the server, use variable count_traversal
     local count_traversal=0
     for server in ${server_arr[*]}
     do
@@ -67,32 +67,34 @@ function collect_logfile() {
         server_port=${server_split[1]}
 
         count_traversal=`expr ${count_traversal} + 1`
-        echo -e "\nStart to collect the log of the ${count_traversal} server:\nServer_ip:${server_ip}\nServer_port:${server_port}\n"
+        echo "--------------------------------------"
+        echo -e "Collect log files from:\nServer Machine: ${count_traversal}\nServer Ip: ${server_ip}\nServer Port: ${server_port}"
 
         # Check if IP:PORT can connect, timeout 1 seconds
-        telnet_return=`nc -w 1 ${server_ip} ${server_port} && echo yes || echo no`
+        nc_return=`nc -w 1 ${server_ip} ${server_port} && echo yes || echo no`
 
-        if [ "x${telnet_return}" == "xyes" ]; then
-            echo "New logfile save folder, named: ip-port"
-    	    mkdir -p ${work_path}/${server_ip}-${server_port}
-    	    cd ${work_path}/${server_ip}-${server_port}
+        if [ "x${nc_return}" == "xyes" ]; then
+            # New logfile save folder, named: ip-port
+            mkdir -p ${work_path}/${server_ip}-${server_port}
+            cd ${work_path}/${server_ip}-${server_port}
     
-            # Count collecting log on the server, use variable m
+            # Count collecting log on the server, use variable count_logfile
             local count_logfile=0
             # Cycle logfile_list
             for logfile in ${logfile_list[*]}
             do
                 count_logfile=`expr ${count_logfile} + 1`
-                echo -e "\nStart to collect logfile:${count_logfile}\n"
+                echo "======================================"
+                echo "Collect log files:${count_logfile}"
 
                 # Get server_ip hostname
-                server_hostname=`ssh -i ${ssh_key_file} -p ${server_port}  -o StrictHostKeyChecking=no root@${server_ip} "hostname"`
+                server_hostname=`ssh -i ${ssh_key_file} -p ${server_port} -o StrictHostKeyChecking=no root@${server_ip} "hostname"`
                 
                 # By connect ip collect logfile
-                echo -e "\nCurrent collection of log information:\nHostname:${server_hostname}\nServer_ip:${server_ip}\nServer_port:${server_port}\nLogfile: $logfile\n"
+                echo -e "\nCurrent collection of log information:\nServer Hostname: ${server_hostname}\nServer Ip: ${server_ip}\nServer Port: ${server_port}\nLog file: $logfile\n"
     
                 # True if logfile exists and is readable
-                ssh_result=`ssh -i ${ssh_key_file} -p ${server_port}  -o StrictHostKeyChecking=no root@${server_ip} "test -r ${logfile}" && echo yes || echo no`
+                ssh_result=`ssh -i ${ssh_key_file} -p ${server_port} -o StrictHostKeyChecking=no root@${server_ip} "test -r ${logfile}" && echo yes || echo no`
     
                 # If exist and readable
                 if [ "x${ssh_result}" == "xyes" ]; then
@@ -103,38 +105,40 @@ function collect_logfile() {
     
                     # ${tail_line} less equal 0 or null
                     if [ $tail_line -le 0 ] || [ -z $tail_line ];then
-                        log "Please refer to the correct parameters for the prompt configuration"
+                        log "Error: Invalid parameter for ${tail_line}. It should be positive integer"
+                        exit 1
                     else
                         # collect the tail line of the log file.
                         ssh -i ${ssh_key_file} -p ${server_port} -o StrictHostKeyChecking=no root@${server_ip} "tail -n ${tail_line} ${logfile}" > ./${logfile_parent_dir}/${logfile_name}
                     fi
     	        else
-                    echo -e "\nThe logfile:${logfile}\nOn the server[ip:port-${server_ip}:${server_port}] is not readable or does not exist\n"
+                    echo -e "Warning: The log files:\n${logfile}\nIs not present or not readable on the Server:\nIp:Port-${server_ip}:${server_port}\n"
                 fi
             done
 
-            if [ `ls | wc -l` -ge 0 ]; then
+            if [ `ls | wc -l` -gt 0 ]; then
                 # Pack all the log files in the server
                 cd ${work_path}
                             
                 # compress current named:hostname-server_ip-server_port-current_time logfile
+                echo "Tar current Server:${server_ip}:${server_port} collected log files:"
                 tar -zcvf ${server_hostname}-${server_ip}-${server_port}-${collect_time}.tar.gz ${server_ip}-${server_port}/*
             else
-                echo -e "\nThe ${server_ip}-${server_port} folder is empty,did not collect the log file\n"
+                echo -e "\nWarning: The ${server_ip}-${server_port} folder is empty,did not collect the log file\n"
             fi
 
             # Delete ${server_ip}
             rm -rf ${server_ip}-${server_port}
         else
-            echo -e "\nCan not collect logfile in the:[ServerIp:Port:${server_ip}:${server_port}]\nNotice:Please check if the network is connected or otherwise\n"
+            echo -e "\nWarning:Can not collect the log file in the Server:${server_ip}:${server_port}\nNotice:Please check if the network is connected or otherwise\n"
         fi
     done
     
-    if [ `ls | wc -l` -ge 0 ]; then
+    if [ `ls | wc -l` -gt 0 ]; then
         # Download logfile
-        echo -e "\nDownload log package link:${JOB_URL}/ws\n"
+        echo -e "\nGenerate log files download link:\n${JOB_URL}/ws\n"
     else
-        echo -e "\nSorry, Log packege is empty!\n"
+        echo -e "\nWarning: Log packege is empty!\n"
     fi
 }
 
@@ -154,5 +158,12 @@ cd ${work_path}
 collect_logfile
 
 # Delete retention day tar
-find ${WORKSPACE} -mtime +${retention_day} -name "${JOB_NAME}*" -exec rm -rf {} \+
+find_return=`find ${WORKSPACE} -mtime ${retention_day} -name "${JOB_NAME}*"`
+stat_return=`echo "${find_return}" | grep ${JOB_NAME}*`
+if [ $? -eq 0 ]; then
+    find ${WORKSPACE} -mtime ${retention_day} -name "${JOB_NAME}*" -exec rm -rf {} \+
+    echo -e "\nWarning: You have deleted the folder:\n${find_return}"
+else
+    echo -e "\nNotice: You did not delete any folders"
+fi
 ############################ collect_logfile.sh End #################################
